b5977184c72e19210c3b2073a642a50c3baac0ef
Sam Blackburn <samblackburna@gmail.com>
2026-05-06 17:13:39 -0600

    completed knight

---
+++ notes.md
@@ -0,0 +1,13 @@
+# My notes
+Jumping into this class late, hoping you show me a little mercy on the late penalty?
+
+
+
+Going forward, this can be a location for notetaking on the "techniques and technologies" that I learn.
+
+Notes 5/4:
+Standard git process:
+  echo "# My notes" > notes.md                  <- (creates the file)
+  git add notes.md
+  git commit -am "initial(notes) creation"
+  git push
---
+++ shared/src/main/java/chess/ChessPiece.java
@@ -0,0 +1,158 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents a single chess piece
+ * <p>
+ * Note: You can add to this class, but you may not alter
+ * signature of the existing methods.
+ */
+public class ChessPiece {
+
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    /**
+     * The various different chess piece options
+     */
+    public enum PieceType {
+        KING,
+        QUEEN,
+        BISHOP,
+        KNIGHT,
+        ROOK,
+        PAWN
+    }
+
+    /**
+     * @return Which team this chess piece belongs to
+     */
+    public ChessGame.TeamColor getTeamColor() {
+        return pieceColor;
+    }
+
+    /**
+     * @return which type of chess piece this piece is
+     */
+    public PieceType getPieceType() {
+        return type;
+    }
+
+    /**
+     * Calculates all the positions a chess piece can move to
+     * Does not take into account moves that are illegal due to leaving the king in
+     * danger
+     *
+     * @return Collection of valid moves
+     */
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            Collection<ChessMove> moves = new ArrayList<>();
+            int[][] directions = {{1,1},{1,-1},{-1,1},{-1,-1}};
+            for (int[] dir : directions) {
+                int row = myPosition.getRow();
+                int column = myPosition.getColumn();
+
+                while (true) {
+                    row += dir[0];
+                    column += dir[1];
+
+                    if (row < 1 || row > 8 || column < 1 || column > 8) break;
+
+                    ChessPosition newPosition = new ChessPosition(row, column);
+                    ChessPiece target = board.getPiece(newPosition);
+                    if (target == null) {
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    } else {
+                        if (target.getTeamColor() != piece.getTeamColor()) {
+                            moves.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                        break;
+                    }
+                }
+            }
+            return moves;
+            // return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1, 8), null));
+        } else if (piece.getPieceType() == PieceType.PAWN) {
+
+        }
+        else if (piece.getPieceType() == PieceType.KNIGHT) {
+            Collection<ChessMove> moves = new ArrayList<>();
+            for (int row = 1; row <= 8; row++) {
+                for (int column = 1; column <= 8; column++) {
+                    int rowDiff = Math.abs(row - myPosition.getRow());
+                    int columnDiff = Math.abs(column - myPosition.getColumn());
+
+                    ChessPosition newPosition = new ChessPosition(row, column);
+                    ChessPiece target = board.getPiece(newPosition);
+                    if ((rowDiff == 2 && columnDiff == 1) || (rowDiff == 1 && columnDiff == 2)) {
+                        if (target == null) {
+                            moves.add(new ChessMove(myPosition, newPosition, null));
+                        } else {
+                            if (target.getTeamColor() != piece.getTeamColor()) {
+                                moves.add(new ChessMove(myPosition, newPosition, null));
+                            }
+                        }
+                    }
+                }
+            }
+            return moves;
+        }
+        else if (piece.getPieceType() == PieceType.ROOK) {
+            Collection<ChessMove> moves = new ArrayList<>();
+            int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
+            for (int[] dir : directions) {
+                int row = myPosition.getRow();
+                int column = myPosition.getColumn();
+
+                while (true) {
+                    row += dir[0];
+                    column += dir[1];
+
+                    if (row < 1 || row > 8 || column < 1 || column > 8) break;
+
+                    ChessPosition newPosition = new ChessPosition(row, column);
+                    ChessPiece target = board.getPiece(newPosition);
+                    if (target == null) {
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    } else {
+                        if (target.getTeamColor() != piece.getTeamColor()) {
+                            moves.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                    }
+                }
+            }
+        }
+        else if (piece.getPieceType() == PieceType.QUEEN) {
+
+        }
+        else if (piece.getPieceType() == PieceType.KING) {
+
+        }
+        return List.of();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+}
