8e8891354d40ac4454c0e3bab252c00f61cb4165
Sam Blackburn <samblackburna@gmail.com>
2026-05-21 23:11:48 -0600

    getting clear working

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -1,21 +1,33 @@
 package service;
 
 import dataaccess.*;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*; // jupiter API
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ServiceTests {
     private DataAccess dataAccess;
     private UserService userService;
+    private ClearService clearService;
 
+    @BeforeEach
     void setUp() {
         dataAccess = new MemoryDataAccess();
         userService = new UserService(dataAccess);
+        clearService = new ClearService(dataAccess);
     }
 
+    @Test
     void registerSuccess() throws DataAccessException {
         var result = userService.register(new UserService.RegisterRequest("sam", "pass", "sam@mail.com"));
         assertEquals("sam", result.username());
         assertNotNull(result.authToken());
+    }
+
+    @Test
+    void clearSuccess() throws DataAccessException {
+        userService.register(new UserService.RegisterRequest("sam", "pass", "sam@mail.com"));
+        clearService.clear();
+        assertNull(dataAccess.getUser("sam"));
     }
 }
