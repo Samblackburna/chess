3cfb6f38e238fd0ec0d38a7fde0c531fcade3800
Sam Blackburn <samblackburna@gmail.com>
2026-05-21 20:02:55 -0600

    rest of data + Memory implementation

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -5,11 +5,14 @@ import java.util.*;
 
 public class MemoryDataAccess implements DataAccess {
     private final Map<String, UserData> users = new HashMap<>();
-
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
     private int nextGameID = 1;
 
     public void clear() {
         users.clear();
+        games.clear();
+        auths.clear();
         nextGameID = 1;
     }
 
@@ -24,5 +27,41 @@ public class MemoryDataAccess implements DataAccess {
         return users.get(username);
     }
 
+    public int createGame(GameData game) throws DataAccessException {
+        int id = nextGameID++;
+        games.put(id, new GameData(id, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game()));
+        return id;
+    }
 
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    public Collection<GameData> listGames() throws DataAccessException {
+        return games.values();
+    }
+
+    // void bc no return value
+    public void updateGame(GameData game) throws DataAccessException {
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game not found - " + game.gameID());
+        }
+        games.put(game.gameID(), game);
+    }
+
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (!auths.containsKey(authToken)) {
+            throw new DataAccessException("Authentcation token not found");
+        }
+        auths.remove(authToken);
+    }
 }
