2e8a131880204119e9925c2c27b7ede986725184
Sam Blackburn <samblackburna@gmail.com>
2026-05-21 20:50:51 -0600

    userService and extra exceptions

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,12 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.*;
+import model.*;
+import java.util.Collection;
+
+public class GameService {
+    private final DataAccess dataAccess;
+
+    
+}
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -2,10 +2,38 @@ package service;
 
 import dataaccess.*;
 import model.*;
-
+import java.util.UUID;
 
 public class UserService {
     private final DataAccess dataAccess;
 
+    public UserService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public record RegisterResult(String username, String authToken) {}
+
+
+    public record RegisterRequest(String username, String password, String email) {}
+    // public record LoginRequest(String username, String password) {}
+    // public record LoginResult(String username, String authToken) {}
+
+
+
+    public RegisterResult register(RegisterRequest req) throws DataAccessException {
+        if (req.username() == null || req.password() == null || req.email() == null) {
+            throw new BadRequestException("bad request");
+        }
+        if (dataAccess.getUser(req.username()) != null) {
+            throw new AlreadyTakenException("already taken");
+        }
+
+        dataAccess.createUser(new UserData(req.username(), req.password(), req.email()));
+
+        String token = UUID.randomUUID().toString();
+        dataAccess.createAuth(new AuthData(token, req.username()));
+        return new RegisterResult(req.username(), token);
+    }
     
+
 }
