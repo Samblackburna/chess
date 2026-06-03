8035154b72738bc09893d8d34ff12013ec7af093
Sam Blackburn <samblackburna@gmail.com>
2026-06-03 16:41:42 -0600

    authToken tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,16 +3,18 @@ package client;
 import org.junit.jupiter.api.*;
 import server.Server;
 
-
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        // specific to extra facade package we added
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +22,53 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws Exception {
+        facade.clearDatabase();
+    }
+
+    // register
+    @Test
+    void registerSuccess() throws Exception {
+        var auth = facade.register("sam", "password", "sam@email.com");
+        Assertions.assertNotNull(auth.authToken());
+        Assertions.assertEquals("sam", auth.username());
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void registerDuplicateUsername() throws Exception {
+        facade.register("sam", "password", "sam@email.com");
+        Assertions.assertThrows(Exception.class, () ->
+                facade.register("sam", "other", "other@email.com"));
     }
+
+    // login
+    @Test
+    void loginSuccess() throws Exception {
+        facade.register("sam", "password", "sam@email.com");
+        var auth = facade.login("sam", "password");
+        Assertions.assertNotNull(auth.authToken());
+        Assertions.assertEquals("sam", auth.username());
+    }
+    @Test
+    void loginWrongPassword() {
+        Assertions.assertThrows(Exception.class, () ->
+                facade.login("sam", "wrongpassword"));
+    }
+
+    // logout
+    @Test
+    void logoutSuccess() throws Exception {
+        var auth = facade.register("sam", "password", "sam@email.com");
+        Assertions.assertDoesNotThrow(() -> facade.logout(auth.authToken()));
+    }
+
+    @Test
+    void logoutInvalidToken() {
+        Assertions.assertThrows(Exception.class, () ->
+                facade.logout("not-a-real-token"));
+    }
+
+
 
 }
