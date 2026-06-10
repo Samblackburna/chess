7dc24beb0886f1e04a79046b25e871995ad39b3b
Sam Blackburn <samblackburna@gmail.com>
2026-06-09 23:15:14 -0600

    just getting WebSocketHandler to work w/out errors

---
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -0,0 +1,64 @@
+package server.websocket;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import io.javalin.websocket.WsCloseContext;
+import io.javalin.websocket.WsCloseHandler;
+import io.javalin.websocket.WsConnectContext;
+import io.javalin.websocket.WsConnectHandler;
+import io.javalin.websocket.WsMessageContext;
+import io.javalin.websocket.WsMessageHandler;
+import model.AuthData;
+import model.GameData;
+import websocket.commands.UserGameCommand;
+import websocket.messages.LoadGameMessage;
+
+public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final DataAccess dataAccess;
+    private final ConnectionManager connections = new ConnectionManager();
+    private static final Gson GSON = new Gson();
+
+    public WebSocketHandler(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void handleConnect(WsConnectContext ctx) {
+    }
+
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            UserGameCommand command = GSON.fromJson(ctx.message(), UserGameCommand.class);
+            switch (command.getCommandType()) {
+                case CONNECT -> connect(ctx, command);
+                case MAKE_MOVE -> makeMove(ctx, command);
+                case LEAVE -> leave(ctx, command);
+                case RESIGN -> resign(ctx, command);
+            }
+        } catch (Exception e) {}
+    }
+
+    public void handleClose(WsCloseContext ctx) {
+    }
+
+    private void connect(WsMessageContext ctx, UserGameCommand command) throws Exception {
+        AuthData auth = dataAccess.getAuth(command.getAuthToken());
+        GameData game = dataAccess.getGame(command.getGameID());
+
+        connections.add(command.getGameID(), auth.username(), ctx.session);
+
+        String loadGameJson = GSON.toJson(new LoadGameMessage(game.game()));
+        connections.sendToUser(auth.username(), loadGameJson);
+
+    }
+
+    private void makeMove(WsMessageContext ctx, UserGameCommand command) throws Exception {
+    }
+
+    private void leave(WsMessageContext ctx, UserGameCommand command) throws Exception {
+    }
+
+    private void resign(WsMessageContext ctx, UserGameCommand command) throws Exception {
+    }
+}
+
